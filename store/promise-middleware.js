c5e5a70a490c6fee3ac66f57316b9758989f3e97
Shane Moyo <smoyo@pdx.edu>
2018-01-07 19:11:04 -0800

    create promise-middleware.js file in store dir

---
+++ store/promise-middleware.js
@@ -0,0 +1,26 @@
+import { LOADING, DONE_LOADING, ERROR } from '../services/constants';
+const isPromise = val => val && typeof val.then === 'function';
+
+export default ({ dispatch }) => next => async action => {
+  
+  if(!isPromise(action.payload)) return next(action);
+
+  const { type, payload } = action;
+  dispatch({ type: LOADING });
+  
+  try {
+    const result = await payload;
+    dispatch({ type: DONE_LOADING });    
+    dispatch({ 
+      type, 
+      payload: result
+    });
+  }
+  catch(err) {
+    dispatch({
+      type: ERROR,
+      payload: err
+    });
+    throw err;
+  }
+};
