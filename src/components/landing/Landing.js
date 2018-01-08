374ac06737d750f91bba690f6cd2d6e3debc5c69
Shane Moyo <smoyo@pdx.edu>
2018-01-07 19:27:53 -0800

    create landing dir and Landing.js and landing.css in landing dir

---
+++ src/components/landing/Landing.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Header from '../header/Header';
+import './landing.css';
+import Navigation from '../navigation/Navigation';
+
+export default class Landing extends Component {
+  
+  render(){
+
+    return(
+        <body>
+          <section class="hero is-info is-fullheight">
+            <Navigation/>
+            <div class="hero-body">
+              <div class="container has-text-centered">
+                <div class="column is-6 is-offset-3">
+                  <h1 class="title">
+                    Coming Soon
+                  </h1>
+                  <h2 class="subtitle">
+                    Welcome to Healthihost.  
+                  </h2>
+              </div>
+            </div>
+          </div>
+      </section>
+    </body>
+    );
+  }
+}
