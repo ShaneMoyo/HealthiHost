ac1f36a83286630b1ac2f8a3ecdb21bead62ae97
Shane Moyo <smoyo@pdx.edu>
2018-01-07 19:31:19 -0800

    added Navigation an landing component to app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Landing from './landing/Landing';
+import Navigation from './navigation/Navigation';
+import Massage from './massage/Massage';
+import Minerals from './minerals/Minerals';
+import Movement from './movement/Movement';
+import { 
+  BrowserRouter as Router, 
+  Route,
+  Switch,
+  Redirect 
+} from 'react-router-dom';
 import './App.css';
 
+
+
 class App extends Component {
+ 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <head>
+            <meta charset="utf-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <title>HealthiHost</title>
+          </head>
+            <Switch>
+              <Route exact path="/" component={Landing}/>
+              <Route exact path="/massage" component={Massage}/>
+              <Route exact path="/minerals" component={Minerals}/>
+              <Route exact path="/movement" component={Movement}/>
+              <Redirect to="/"/>
+            </Switch>   
+        </div>
+      </Router>
     );
   }
 }
+
+
+
 
 export default App;
